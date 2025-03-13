4550bc11bbd57dbc0115a6b52af8c0a30e9d7f70
YoshuaPariona <loganpariona.1207@gmail.com>
2025-03-12 21:23:50 -0500

    Implementar: crear botones para editar o eliminar en cada nombre

--- src/presentation/render-cards.js
+++ src/presentation/render-cards.js
@@ -1,17 +1,15 @@
 export const renderCards = ( cards, amigos ) => {
-    
     cards.innerHTML = '';
-
     let columnCards = createColumn( cards );
 
     amigos.forEach( ( amigo, index ) => {
-        //Cuando existan 4, agrega una columna
         if( index > 0 && index % 4 === 0 ){
             columnCards = createColumn( cards );
         } 
-        const nameCard = createCard( amigo );
+        const nameCard = createCard( amigo, index, amigos, cards );
         columnCards.append(nameCard);
     });
+
 }
 
 const createColumn = ( cards ) => {
@@ -21,9 +19,42 @@ const createColumn = ( cards ) => {
     return column;
 }
 
-const createCard = ( amigo ) =>{
+const createCard = ( amigo, index, amigos, cards ) =>{
     const card = document.createElement('div');
     card.className = 'name-card';
-    card.innerHTML = `<p>${ amigo }</p>`;
+
+    const name = document.createElement('p');
+    name.className = 'name-text';
+    name.innerHTML = amigo;
+
+    const btns = document.createElement('div');
+    btns.className = 'inner-card-buttons';
+
+    const editBtn = document.createElement('img');
+    const eraseBtn = document.createElement('img');
+    editBtn.src = './assets/edit-icon.svg';
+    editBtn.height = 32;
+    eraseBtn.src = './assets/erase-icon.svg';
+    eraseBtn.height = 32;
+
+    btns.append(editBtn, eraseBtn)
+
+    card.append(name, btns);
+
+    editBtn.addEventListener('click', () => {
+        const newName = prompt('Edita el nombre del amigo:', amigo);
+        if( !newName.trim()) {
+            alert('Ingrese un nombre válido');
+            return;
+        }
+        amigos[index] = newName;
+        renderCards( cards, amigos );
+    })
+
+    eraseBtn.addEventListener('click', () => {
+        amigos.splice( index,1 );
+        renderCards( cards, amigos );
+    })
+
     return card;
 }
--- src/presentation/render-result.js
+++ src/presentation/render-result.js
@@ -3,6 +3,4 @@ export const renderResult = ( result ) => {
     const resultTitle = document.querySelector('.result-title');
     resultTitle.innerHTML = `Tu amigo secreto es 
         <span class="result">${ result }</span>` ;
-    
-    result.append(resultTitle);
 }
